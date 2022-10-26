a10067ba18999414d3e8c3173a4e2281fed697ad
jqueen <jqueen@localhost>
2022-10-26 01:24:02 -0400

    fix startup add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Spring boot with MyBatis, Jersey Rest, and SQLite DB
+log4j2
+
+Build and start on localhost:8080/people  
+
+```
+mvn clean spring-boot:run
+```
--- src/main/java/org/cheesypoof/restdemo/RestdemoApplication.java
+++ src/main/java/org/cheesypoof/restdemo/RestdemoApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class RestdemoApplication implements  CommandLineRunner{
 
 	private static final Logger log=  LogManager.getLogger(HelloResource.class);
